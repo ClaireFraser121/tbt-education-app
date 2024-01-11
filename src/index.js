f67ab526d24bcfba433cda38c4147c9f53c30609
Claire <clairefraser121@gmail.com>
2024-01-11 14:53:02 +0000

    Installed bootstrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
