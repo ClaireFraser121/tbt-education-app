69c0c6de85dfb9fb3208136262f4294cbd0254bb
partialarts <145933508+jemlawX@users.noreply.github.com>
2024-01-16 19:16:30 +0000

    testing API

--- src/components/funFacts/FunFacts.jsx
+++ src/components/funFacts/FunFacts.jsx
@@ -29,6 +29,20 @@
 import React from 'react';
 import Tiles from './Tiles'; // Adjust the path based on your project structure
 
+const getRandomFact = async () => {
+  try {
+      const response = await fetch('https://rest.blackhistoryapi.io/fact/random', {
+        method: "GET",
+        headers: { 'X-Api-Key': 'amVtU3VuIEphbiAxNCAyMDI0IDExOj'},
+        contentType: 'application/json',
+      });
+      const data = await response.json();
+      console.log(data)
+  } catch (error) {
+      console.error('Error fetching random fact', error);
+  }
+};
+
 const FunFacts = () => {
   return (
     <div>
