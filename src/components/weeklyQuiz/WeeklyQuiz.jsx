b2985c0e3fd92d1b9ccb35a97aca6abc9f2d0c52
partialarts <145933508+jemlawX@users.noreply.github.com>
2024-01-24 13:05:33 +0000

    Add quiz logic

--- src/components/weeklyQuiz/WeeklyQuiz.jsx
+++ src/components/weeklyQuiz/WeeklyQuiz.jsx
@@ -1,58 +1,33 @@
-// Weekly Quiz Page:
-
-// Quiz Questions:
-//  Create a set of ten quiz questions based on Black History.
-//  Design a layout to present the questions.
-// 
-// Answer Options:
-//  Provide multiple-choice answer options for each question.
-//  Ensure a clear and user-friendly format for answering.
-
-// User Interaction:
-//  Implement a system to track user answers.
-//  Display correct answers after submission.
-
-
-// Pseudocode
-// # Weekly Quiz
-
-// FUNCTION handleAnswer(questionIndex, selectedOption):
-//     SET userAnswers[questionIndex] = selectedOption
-
-// # Main component
-// FUNCTION QuizPage():
-//     SET userAnswers = []
-
-//     PRINT 'Weekly Quiz'
-//     FOR EACH question IN quizQuestions:
-//         PRINT question.text
-//         FOR EACH option IN question.options:
-//             BUTTON 'Option', onClick=handleAnswer(questionIndex, option)
-
-//     PRINT 'User\'s Answers:'
-//     FOR EACH answer IN userAnswers:
-//         PRINT answer
-
-// WeeklyQuiz.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import API from '../../utils/API';
 
 function WeeklyQuiz() {
     const [quizQuestions, setQuizQuestions] = useState([]);
 
-    useEffect(() => {
+      useEffect(() => {
         const fetchData = async () => {
           try {
             const data = await API.getQuizQuestions();
-            setQuizQuestions(data);
+            // Map through the fetched data to format it correctly
+            const formattedData = data.map((item) => ({
+              questionId: item.QuestionID,
+              question: item.Question,
+              answerOptions: [
+                { content: item.OptionA, type: 'OptionA' },
+                { content: item.OptionB, type: 'OptionB' },
+                { content: item.OptionC, type: 'OptionC' },
+              ],
+              correctAnswer: item.CorrectAnswer,
+            }));
+            setQuizQuestions(formattedData);
           } catch (error) {
             console.error('Error fetching quiz questions:', error);
           }
         };
-
+      
         fetchData();
-    }, []);
+      }, []);
+      console.log(quizQuestions)
+    }
 
-    console.log(quizQuestions);
-}
 export default WeeklyQuiz;
