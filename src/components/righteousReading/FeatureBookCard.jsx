e6aea3309045298c5c1c4c24c7cbe29ed5fdb3be
SimonAMcKenzie <simon-mckenzie@hotmail.co.uk>
2024-01-25 00:44:35 +0000

    added responsiveness using tailwind specifically for tablet and mobile

--- src/components/righteousReading/FeatureBookCard.jsx
+++ src/components/righteousReading/FeatureBookCard.jsx
@@ -1,25 +1,24 @@
-// src/components/righteousReading/FeatureBookCard.jsx
 import React from 'react';
 
 const FeatureBookCard = () => {
   return (
-    <div className="hero-section bg-gradient-to-r from-green-800 via-green-600 to-green-400 text-white p-8 mb-8 flex justify-between items-center">
-      <div className="w-1/2">
-        <h2 className="text-4xl font-bold mb-4 ml-20">Freestyle Saga - Part One</h2>
-        <div className="text-description mt-10 ml-20 mr-10">
+    <div className="hero-section bg-gradient-to-r from-green-800 via-green-600 to-green-400 text-white p-8 mb-8 flex flex-col-reverse md:flex-row justify-between items-center">
+      <div className="w-full md:w-1/2">
+        <h2 className="text-4xl font-bold mb-4 md:ml-20">Freestyle Saga - Part One</h2>
+        <div className="text-description mt-10 md:ml-20 md:mr-10">
           <p>
-          In part one of this autobiographical collection of rhymes, '2 Badda' fearlessly ignites discussion and sparks transformative change through his mind-blowing, uncensored freestyles, taking the nation by storm with his unapologetically honest approach and hard-hitting truths. '2 Badda' challenges the status quo, inspiring a revolutionary movement of people who refuse to be silenced. Brace yourself to be taken on a heart-wrenching journey, as you uncover the real stories and life experiences of this remarkable and prolific artist, through his powerful words that will linger long after you turn the final page.
+            In part one of this autobiographical collection of rhymes, '2 Badda' fearlessly ignites discussion and sparks transformative change through his mind-blowing, uncensored freestyles, taking the nation by storm with his unapologetically honest approach and hard-hitting truths. '2 Badda' challenges the status quo, inspiring a revolutionary movement of people who refuse to be silenced. Brace yourself to be taken on a heart-wrenching journey, as you uncover the real stories and life experiences of this remarkable and prolific artist, through his powerful words that will linger long after you turn the final page.
           </p>
         </div>
         <a href="https://2badda.com/products/freestyle-saga-part-one" className="block" target="_blank" rel="noopener noreferrer">
-          <button className="bg-yellow-400 hover:bg-green-400 text-white font-bold py-2 px-4 rounded mt-10 ml-20">
-          Get Book
+          <button className="bg-yellow-400 hover:bg-green-400 text-white font-bold py-2 px-4 rounded mt-10 md:ml-20">
+            Get Book
           </button>
         </a>
       </div>
-      <div className="w-1/2 flex justify-center">
+      <div className="w-full md:w-1/2 flex justify-center">
         <img
-          className="object-cover object-center w-full max-w-xs shadow-md  transition-transform transform hover:scale-105 hover:shadow-lg hover:ring-4 ring-red-400"
+          className="object-cover object-center w-full max-w-xs shadow-md transition-transform transform hover:scale-105 hover:shadow-lg hover:ring-4 ring-red-400"
           src="src/components/righteousReading/assets/images/BlackandYellowGeniusAutobiographyBookCover.png"
           alt="Featured Book"
         />
