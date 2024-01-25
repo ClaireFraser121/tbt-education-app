8d4ba440b874e68aeafe4cfd32e090a0f948013a
partialarts <145933508+jemlawX@users.noreply.github.com>
2024-01-25 17:34:15 +0000

    delete unused code and upload favicon.ico

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import Papa from 'papaparse';
 
 export default {
@@ -19,14 +18,3 @@ export default {
     });
   },
 };
-//   getBooks: function() {
-//     return Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vQIuvyzMCv-41hAAhDwH79Gv9G0D6iJ9Fdt1e7pV2-QLZQwwOR9tLgTcLHXz6K8eRakhIiWdtANwgvJ/pub?output=csv', {
-//         download: true,
-//         header: true,
-//         complete: function(results) {
-//           var data = results.data
-//           console.log(data)
-//         }
-//       })
-//       }
-// };
