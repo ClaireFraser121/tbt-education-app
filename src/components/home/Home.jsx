e1c67257f81388388c059e8a02ea1a30d54e1832
Claire <clairefraser121@gmail.com>
2024-01-25 14:58:24 +0000

    reduced margins for better a appearance

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,7 +7,7 @@ import './Home.css';
 const Home = () => {
   return (
     <>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
         <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
           <div className="relative lg:w-1/2">
             <img
@@ -58,7 +58,7 @@ const Home = () => {
       </div>
 
       <div className="bg-gray-900">
-        <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
           <div className="absolute inset-x-0 top-0 items-center justify-center hidden overflow-hidden md:flex md:inset-y-0">
             <svg
               viewBox="0 0 88 88"
