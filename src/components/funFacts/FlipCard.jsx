72a865fb1df9bd1bd3c3333a69798b6d8d66f0f1
Claire <clairefraser121@gmail.com>
2024-01-26 12:25:43 +0000

    clean up my code

--- src/components/funFacts/FlipCard.jsx
+++ src/components/funFacts/FlipCard.jsx
@@ -6,23 +6,31 @@ import axios from 'axios';
 import './flipCard.css';
 
 const FlipCard = ({ person }) => {
-  console.log('Received person object:', person);
+  // Received person object
+ 
+
+  // State for flipped status and fact
   const [isFlipped, setIsFlipped] = useState(false);
   const [fact, setFact] = useState(person.fact);
 
+  // Animation controls
   const controls = useAnimation();
 
+  // Handle click to flip the card
   const handleClick = async () => {
+    // Fetch a new fact if not flipped
     if (!isFlipped) {
-      const newFact = await fetchNewFact(person.name); // Pass person.name instead of the whole person object
+      const newFact = await fetchNewFact(person.name);
       setFact(newFact);
     }
 
+    // Animate flip
     await controls.start({ rotateY: isFlipped ? 0 : 180 });
     setIsFlipped(!isFlipped);
   };
 
-  const fetchNewFact = async (personName) => { // Change parameter name
+  // Fetch a new fact for the specified person
+  const fetchNewFact = async (personName) => {
     try {
       const response = await axios.get(`https://rest.blackhistoryapi.io/fact?people=${personName}`, {
         headers: { 'X-Api-Key': 'amVtU3VuIEphbiAxNCAyMDI0IDExOj' },
@@ -37,36 +45,36 @@ const FlipCard = ({ person }) => {
     }
   };
 
- // console.log(`person.imageFileName: ${JSON.stringify(person)}`);
-
-
- return (
-  <div className="card-container-item">
-    <motion.div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick} animate={controls}>
-      <motion.div className="card-face front">
-        <div
-          className="image-container"
-          style={{
-            backgroundImage: `url(${person.imagePath})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundColor: 'whitesmoke',
-            width: '100%',
-            height: '100%',
-          }}
-        />
-        <p>{person.name}</p>
-      </motion.div>
-      {isFlipped && (
-        <motion.div className="card-face back">
-          <p style={{ color: 'Black' }}>{fact}</p>
+  // Render the card
+  return (
+    <div className="card-container-item">
+      {/* Card with motion animations */}
+      <motion.div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick} animate={controls}>
+        {/* Front face of the card */}
+        <motion.div className="card-face front">
+          {/* Image container */}
+          <div
+            className="image-container"
+            style={{
+              backgroundImage: `url(${person.imagePath})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundColor: 'whitesmoke',
+              width: '100%',
+              height: '100%',
+            }}
+          />
+          <p>{person.name}</p>
         </motion.div>
-      )}
-    </motion.div>
-  </div>
-  
-);
+        {/* Back face of the card */}
+        {isFlipped && (
+          <motion.div className="card-face back">
+            <p style={{ color: 'Black' }}>{fact}</p>
+          </motion.div>
+        )}
+      </motion.div>
+    </div>
+  );
 };
 
 export default FlipCard;
-
